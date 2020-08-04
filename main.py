94f7cebb76f0fc1f2fa9e6e7f54a5e2e7f4f5a92
EthanCabon <ethan.cabon@grenoble-inp.org>
2020-08-04 14:45:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from .problem import *
+from problem import *
 
 # Nothing in the main yet
 
 cwd = os.getcwd()
 
-deck = Deck(cwd + "/deck.yaml")
+deck = Deck(cwd + "/deck_printing3D.yaml")
+
+geometry = Geometry_Printing3D(deck)
+
+meshing = Meshing_Layer(deck, geometry)
